a8effa25e67f32ccb67020bdba5ac10f4f643aed
Balazs Pukli <balazsptm@gmail.com>
2018-10-25 07:32:08 +0200

    beta

---
+++ precache-manifest.0ce33e753310a6a25c7851457036e149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02cc94a4666e00ee81b7",
+    "url": "/static/js/main.02cc94a4.chunk.js"
+  },
+  {
+    "revision": "28cff4f89b1961254165",
+    "url": "/static/js/1.28cff4f8.chunk.js"
+  },
+  {
+    "revision": "02cc94a4666e00ee81b7",
+    "url": "/static/css/main.8e084b9c.chunk.css"
+  },
+  {
+    "revision": "2f7a3abcd3e3f0908bdb8a8873a14119",
+    "url": "/index.html"
+  }
+];
